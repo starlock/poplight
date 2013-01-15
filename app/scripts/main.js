811133db4a8dd536abc03534bdd5c4760bbad133
Sven Widen <sven.widen@gmail.com>
2013-01-15 18:35:20 +0100

    ugly hack

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,11 @@ window.poplight = {
   init: function() {
     var request = new API.Request('marketplace.popular.products', {}, 'GET');
     request.setOnSuccess(function(products) {
-			console.log(products);
+			_.each(products, function(product) {
+				$('#product_list').append('<li class="product_item media">'
+				+ '<img src="' + product.images[0].sizes["50"]  + '" /><span>'
+				+ product.title + '</span></li>');
+			});
     });
     request.send();
   }
